029140c44244156c82e7024ab475b2afc42152b3
franck <besson.franck@imerir.com>
2017-06-26 16:24:17 +0200

    close #2 R1 Done

--- players_post.py
+++ players_post.py
@@ -131,16 +131,16 @@ def create_player_by_name(player_name):
 # Requête pour log un utilisateur
 def players_post_request(elements):
 
-	name = str(elements["name"])
+  name = str(elements["name"])
 
-    if player_exist(name) == False :
+  if player_exist(name) == False :
 
-    	create_player_by_name(name)
+    create_player_by_name(name)
 
-  	response = {
-  	  "name" : name,
-  	  "location" : get_player_location_by_player_name(name),
-  	  "info" : get_player_info_by_player_name(name)
-  	}
+  response = {
+    "name" : name,
+    "location" : get_player_location_by_player_name(name),
+    "info" : get_player_info_by_player_name(name)
+  }
 
-	return json.dumps(response), 200, { "Content-Type": "application/json" }
+  return json.dumps(response), 200, { "Content-Type": "application/json" }
