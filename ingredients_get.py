eed0aeb912985a399487e511ae1c257818f3c159
Louis <louis.schneider@imerir.com>
2017-06-27 10:40:45 +0200

    Submitting function get ingredient for test

--- ingredients_get.py
+++ ingredients_get.py
@@ -4,4 +4,26 @@ from db import Db
 import json
 
 def ingredients_get_request():
-	return json.dumps(""), 200, { "Content-Type": "application/json" }
+	int i =0
+
+	db_recipe_response = db.select("""
+        SELECT *
+        FROM RECIPE
+		WHERE recipe_name<>compose_recipe_name
+        ORDER BY recipe_name;
+      """)
+
+	ingredients = db_recipe_response[0]
+
+	ingredientInfo = {
+	  "name" : db_recipe_response[0]["recipe_name"],
+	  "price" : get_recipe_produce_price_by_name(db_recipe_response[0]["recipe_name"]),
+	  "hasAlcohol" : db_recipe_response[0]["recipe_alcohol"],
+	  "isCold" : db_recipe_response[0]["recipe_cold"]
+	}
+
+	while ():
+		ingredientList[i] = db_recipe_response[i]["recipe_name"]
+		i++
+
+	return json.dumps(ingredientList), 200, { "Content-Type": "application/json" }
