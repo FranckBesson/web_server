d110c0d048aa704f90e48d52cf6953d05e2af350
franck <besson.franck@imerir.com>
2017-06-27 15:33:20 +0200

    test #5 R4

--- ingredients_get.py
+++ ingredients_get.py
@@ -1,22 +1,22 @@
 # coding=utf-8
 
-from db import Db
-import json
-
-db = Db()
-
-def ingredients_get_request():
-	i =0
-
-	# Faire la requêtes pour récupérer les recette dont le nom apparait comme compose_ingredient_recipe_name dans compose
-	db_recipe_response = db.select("""
-        SELECT compose_ingredient_recipe_name
-        FROM compose
-		WHERE recipe_name<>compose_recipe_name
-        ORDER BY recipe_name;
-      """)
-
-	for recipe in db_recipe_response :
-		ingredientList.append(recipe)
-
-	return json.dumps(ingredientList), 200, { "Content-Type": "application/json" }
+# from db import Db
+# import json
+# 
+# db = Db()
+# 
+# def ingredients_get_request():
+# 	i =0
+# 
+# 	# Faire la requêtes pour récupérer les recette dont le nom apparait comme compose_ingredient_recipe_name dans compose
+# 	db_recipe_response = db.select("""
+#         SELECT compose_ingredient_recipe_name
+#         FROM compose
+# 		WHERE recipe_name<>compose_recipe_name
+#         ORDER BY recipe_name;
+#       """)
+# 
+# 	for recipe in db_recipe_response :
+# 		ingredientList.append(recipe)
+# 
+# 	return json.dumps(ingredientList), 200, { "Content-Type": "application/json" }
