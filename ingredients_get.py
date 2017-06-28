dd665eb42e3ac80e6be95308c3db039877d44ff7
Unknown <fabien.mazette@gmail.com>
2017-06-28 12:02:16 +0200

    Ingredient_get.py done

--- ingredients_get.py
+++ ingredients_get.py
@@ -7,13 +7,11 @@ db = Db()
 
 def ingredients_get_request():
 	# récupération des valeurs
-	compose = get_compose()
-	receipe = get_receipe()
+	ingredients = get_ingredients()
 
 	#création de l'ingrédient _ing
 	_ing = {
-	"compose" = compose
-	"receipe" = receipe
+	"ingredients" = ing
 	}
 
 	#Création de l'objet à renvoyer
@@ -26,11 +24,19 @@ def ingredients_get_request():
 
 	return json.dumps(ingredientList), 200, { "Content-Type": "application/json" }
 
-def get_compose ()
-db_compose = db.select("""
-	SELECT DISTINCT compose_ingredients_receipe_name
-	FROM compose """)
+def get_ingredients ():
+	db_compose = db.select("""
+		SELECT DISTINCT compose_ingredients_recipe_name
+		FROM compose """)
 
-db_receipe = db.select("""
-	SELECT*
-	FROM RECIPE)
+	db_recipe = db.select("""
+		SELECT*
+		FROM RECIPE """)
+
+	tab = []
+
+	for recipe in db_recipe
+		for ingredient in db_compose
+			if recipe["recipe_name"] == compose["compose_ingredients_recipe_name"]
+				 tab.append(recipe["recipe_name"])
+	return tab
