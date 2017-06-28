aa0edf03845a38dfce89e68b2ac53b7503a29e87
franck <besson.franck@imerir.com>
2017-06-28 14:42:15 +0200

    ingredients root open

--- sales_post.py
+++ sales_post.py
@@ -5,18 +5,20 @@ import json
 
 def sales_post_request(elements):
 
-  print(str(elements))
+  sales = elements["sales"]
 
-  sale_player_name = str(elements["sales"]["player"])
-  item = str(elements["sales"]["item"])
-  quantity = str(elements["sales"]["quantity"])
+  for sale in sales :
 
-  db.execute("""
-    UPDATE SALE
-    SET sale_number = """+quantity+"""
-    WHERE sale_day_number = """+str(get_current_day_number())+"""
-    AND sale_recipe_name = '"""+item+"""'
-    AND sale_player_name = '"""+sale_player_name+"""';
-  """)
+  	sale_player_name = str(sale["player"])
+  	item = str(sale["item"])
+  	quantity = str(sale["quantity"])
+
+  	db.execute("""
+    	UPDATE SALE
+    	SET sale_number = """+quantity+"""
+    	WHERE sale_day_number = """+str(get_current_day_number())+"""
+    	AND sale_recipe_name = '"""+item+"""'
+    	AND sale_player_name = '"""+sale_player_name+"""';
+  	""")
 
   return json.dumps(""), 200, { "Content-Type": "application/json" }
