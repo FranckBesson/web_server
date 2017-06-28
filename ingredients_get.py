f959a634bc7c65d6815bfda9f46e14213a503b2c
franck <besson.franck@imerir.com>
2017-06-28 09:57:32 +0200

    #6 R5

--- ingredients_get.py
+++ ingredients_get.py
@@ -6,31 +6,31 @@ import json
 db = Db()
 
 def ingredients_get_request():
-#	# récupération des valeurs
-#	compose = get_compose()
-#	receipe = get_receipe()
-#
-#	#création de l'ingrédient _ing
-#	_ing = {
-#	"compose" = compose
-#	"receipe" = receipe
-#	}
-#
-#	#Création de l'objet à renvoyer
-#	response = {
-#	"ingredients" : _ing
-#	}
-#
-#	for recipe in db_recipe_response :
-#		ingredientList.append(recipe)
-#
-#	return json.dumps(ingredientList), 200, { "Content-Type": "application/json" }
-#
-#def get_compose ()
-#db_compose = db.select("""
-#	SELECT DISTINCT compose_ingredients_receipe_name
-#	FROM compose """)
-#
-#db_receipe = db.select("""
-#	SELECT*
-#	FROM RECIPE)
+	# récupération des valeurs
+	compose = get_compose()
+	receipe = get_receipe()
+
+	#création de l'ingrédient _ing
+	_ing = {
+	"compose" = compose
+	"receipe" = receipe
+	}
+
+	#Création de l'objet à renvoyer
+	response = {
+	"ingredients" : _ing
+	}
+
+	for recipe in db_recipe_response :
+		ingredientList.append(recipe)
+
+	return json.dumps(ingredientList), 200, { "Content-Type": "application/json" }
+
+def get_compose () :
+	db_compose = db.select("""
+		SELECT DISTINCT compose_ingredients_receipe_name
+		FROM compose;""")
+
+	db_receipe = db.select("""
+		SELECT*
+		FROM RECIPE;""")
