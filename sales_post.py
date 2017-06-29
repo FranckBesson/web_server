3260dae8236de54a3edf6c61488b06188ae3bff0
franck <besson.franck@imerir.com>
2017-06-30 01:47:19 +0200

    correct bug

--- sales_post.py
+++ sales_post.py
@@ -17,12 +17,12 @@ def sales_post_request(elements):
 	current_day = get_current_day_number()
 
 	# On caste la valeur de quantity pour pas que le joueur ne vende plus de boisson
-	cast_quantity_value_for_sale(str(sale["quantity"]),current_day,str(sale["item"]),str(sale["player"]))
+	new_quantity = cast_quantity_value_for_sale(float(sale["quantity"]),current_day,str(sale["item"]),str(sale["player"]))
 
 	# Mise à jour de la table sale pour prendre en compte les ventes
 	db.execute("""
 		UPDATE SALE
-		SET sale_number = """+str(sale["quantity"])+"""
+		SET sale_number = """+str(new_quantity)+"""
 		WHERE sale_day_number = """+str(current_day)+"""
 		AND sale_recipe_name = '"""+str(sale["item"])+"""'
 		AND sale_player_name = '"""+str(sale["player"])+"""';
